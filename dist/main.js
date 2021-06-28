8426cd137c356d97ff098101cce6d5bf66a9326d
Jack malakhov <jack.malakhov@gmail.com>
2021-06-28 16:38:07 +0800

    add pictures and reusable getEmail block

---
+++ dist/main.js
@@ -0,0 +1,17 @@
+
+function getEmail(a){
+    document.getElementById('start__'+a).style.display = 'none';
+    document.getElementById('end__'+a).style.display = 'grid';
+}
+
+function inputCheck(a){
+    let getInput = document.getElementById('email__'+a);
+    let getlabel = document.getElementById('label__'+a);
+
+    if (getInput.value.length != ""){
+        getlabel.style.color = 'transparent';
+    }
+    else{
+        getlabel.style.color = 'blue'
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const concat =require('gulp-concat')
 const include = require('gulp-file-include')
 const htmlmin = require('gulp-htmlmin')
 const del = require('del')
+const GulpClient = require('gulp')
 const sync = require('browser-sync').create()
 
 
@@ -24,10 +25,26 @@ function html(){
 function scss(){
     return src('src/scss/style.scss')
         .pipe(sass())
-        .pipe(autoprefixer())
-        .pipe(csso())
+        // .pipe(autoprefixer())
+        // .pipe(csso())
         .pipe(concat('index.css'))
         .pipe(dest('dist'))
+}
+
+function js(){
+    return src('src/js/main.js')
+        .pipe(dest('dist'))
+}
+
+function img(){
+    return src('src/assets/images/*')
+        .pipe(dest('dist/images'))
+
+}
+
+function fonts(){
+    return src('src/assets/fonts/*')
+        .pipe(dest('dist/fonts'))
 }
 
 function clear(){
@@ -41,8 +58,9 @@ function serve(){
 
     watch('./src/pug/**/*.pug', series(html)).on('change', sync.reload)
     watch('./src/scss/**/*.scss', series(scss)).on('change', sync.reload)
+    watch('./src/js/**/*.js', series(js)).on('change', sync.reload)
 }
 
-exports.build = series(clear, scss, html)
-exports.serve = series(clear, scss, html, serve)
-exports.scss = scss
+exports.build = series(clear, scss, html, js, img, fonts)
+exports.serve = series(clear, scss, html, js, img, fonts, serve)
+// exports.scss = scss
